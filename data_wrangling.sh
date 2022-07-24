9d9233eecce4cd21ccf4c7ee83f4e7ca9cc98551
afifsahar <67785781+afifsahar@users.noreply.github.com>
2022-07-24 22:02:50 +0700

    Menambahkan Validasi

--- data_wrangling.sh
+++ data_wrangling.sh
@@ -6,4 +6,5 @@ csvgrep -c event_type -m purchase 2019-sample-1.csv > 2019-sample-2.csv |
 sed -i '1s/.*/event_time,event_type,product_id,category_id,brand,price,category,product_name/' 2019-sample-2.csv | 
 sed -i -E 's/[\sUTC]//' 2019-sample-2.csv | 
 sed -i -E 's/[.][a-zA-Z_]+[.][a-zA-Z_]+,[a-zA-Z_]+.[a-zA-Z_]+.|[.][a-zA-Z_]+,[a-zA-Z_]+./,/' 2019-sample-2.csv | 
-csvlook 2019-sample-2.csv | head
+csvlook 2019-sample-2.csv | head |
+cat 2019-sample-2.csv | grep electronics | grep smartphone | awk -F ',' '{print $5}' | sort | uniq -c | sort -nr
