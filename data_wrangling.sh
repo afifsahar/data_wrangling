8b20bf8e5d9701e666c6e4e507ac81ddf86b4a43
afifsahar <67785781+afifsahar@users.noreply.github.com>
2022-07-24 21:27:21 +0700

    Menambahkan Script Pengolah Data

---
+++ data_wrangling.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+trap "set +x; sleep 5; set -x" DEBUG
+csvstack 2019-Nov-sample.csv 2019-Oct-sample.csv > 2019-sample-0.csv | 
+csvcut -c event_time,event_type,product_id,category_id,brand,price,category_code,category_code 2019-sample-0.csv > 2019-sample-1.csv | 
+csvgrep -c event_type -m purchase 2019-sample-1.csv > 2019-sample-2.csv | 
+sed -i '1s/.*/event_time,event_type,product_id,category_id,brand,price,category,product_name/' 2019-sample-2.csv | 
+sed -i -E 's/[\sUTC]//' 2019-sample-2.csv | 
+sed -i -E 's/[.][a-zA-Z_]+[.][a-zA-Z_]+,[a-zA-Z_]+.[a-zA-Z_]+.|[.][a-zA-Z_]+,[a-zA-Z_]+./,/' 2019-sample-2.csv | 
+csvlook 2019-sample-2.csv | head
